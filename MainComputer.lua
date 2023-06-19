829c72cab47ce49d3719492af25b38250dc38f52
Atlasim1 <70275601+Atlasim1@users.noreply.github.com>
2023-06-19 16:39:25 -0400

    Made MainComputer.Lua

---
+++ MainComputer.lua
@@ -0,0 +1,56 @@
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+
+modem.open(6942)
+
+
+while true do
+    -- Fancyness
+    term.setTextColor(colors.yellow)
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Storage System")
+    term.setTextColor(colors.white)
+    print("Type list to get a list of items")
+    print("Request Storage Item\n> ")
+    term.setCursorPos(3,4)
+    -- Get Request
+    local request = io.read()
+    if request == "list" then -- list Items
+        print("this will one day be a list")
+    else
+        print("How Much?\n> ")
+        term.setCursorPos(3,6)
+        local itemquantity = io.read()
+        -- Formatting
+        local requesteditem = itemquantity .. " of " .. request
+        
+        -- Request Item
+        modem.transmit(6924, 6943, requesteditem)
+        print("Requesting for " .. requesteditem)
+        -- Wait For Answer from storage system
+        local waitfor = true
+        while waitfor do -- Wait for Awnser Event
+            event = {os.pullEvent()}
+            local eventtimeout = os.startTimer(3) -- Start Timeout Timer
+            if event[1] == "timer" then -- if Timeout
+                term.setTextColor(colors.red)
+                print("Timed Out, Invalid Item")
+                term.setTextColor(colors.white)
+                waitfor = false
+                break
+            elseif event[1] == "modem_message" and event[5] == requesteditem .. " done" then -- If No Timeout
+                term.setTextColor(colors.green)
+                print("Item Is Being Sent")
+                term.setTextColor(colors.white)
+                waitfor = false
+                break
+            else -- If stray event
+                term.getCursorPos() -- Do Nothing
+
+            end
+        end
+    end
+    sleep(3) -- Wait for User to be able to read before Clearing Screen
+    term.clear()
+    
+end
